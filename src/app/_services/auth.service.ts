91a02a737e4ecc027208bb8c8cec4e96c635177d
aceleghin <alessandro@optix.cloud>
2022-07-25 17:13:09 +0200

    feat: test oauth

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
+import {tokenresp} from "../app.component";
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +10,26 @@ export class AuthService {
   constructor(private http: HttpClient) {
   }
 
-  // r4MaNe7RA9LjvMGwmkhHmm042WM__w
-  login() {
+
+  getToken(code: string) {
+    // grant_type=authorization_code&code=CODE&redirect_uri=URI
 
     const params = new HttpParams()
-      .set('client_id', 'r4MaNe7RA9LjvMGwmkhHmm042WM__w')
-      .set('response_type', 'code')
-      .set('state', 'test')
+      .set('grant_type', 'authorization_code')
+      .set('code', code)
       .set('redirect_uri', 'https://datarx-15696.web.app/')
-      .set('duration', 'permanent')
-      .set('scope', 'mysubreddits')
 
-    return this.http.get(`
-    https://www.reddit.com/api/v1/authorize`, {params})
+    return this.http.post<tokenresp>(`https://www.reddit.com/api/v1/access_token`, {params})
+  }
+
+
+  getSubreddits() {
+    const params = new HttpParams()
+      .set('after', '')
+      .set('before', '')
+      .set('count', 0)
+      .set('limit', 10)
+
+    return this.http.post<tokenresp>(`https://oauth.reddit.com/subreddits/mine/subscriber`, {params})
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,59 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "./_services/auth.service";
+import {HttpParams} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
+
+export class tokenresp {
+  access_token = '';
+  token_type = "bearer";
+  expires_in: any;
+  scope: string = '';
+  refresh_token = '';
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dataRX';
 
-  constructor(private auth: AuthService) {
+  state: string = '';
+  code: string = '';
+  tokenResp = new tokenresp();
+
+  constructor(private auth: AuthService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    // state=test&code=HJDDO_oqhI-JwIS6ZjMtvZ9N40IxYw#_
+    this.code = this.route.snapshot.queryParams['code'];
+    this.state = this.route.snapshot.queryParams['state'];
+
+    if (this.state) {
+      this.auth.getToken(this.code).subscribe((res: tokenresp) => {
+        this.tokenResp = res;
+
+        this.auth.getSubreddits().subscribe(res => {
+          console.log(res)
+        })
+      })
+    }
   }
 
   login() {
-    this.auth.login().subscribe(res => {
-      console.log(res);
-    })
+
+    let params = new HttpParams()
+      .set('client_id', 'Oe50FeFo9UYAazJ_kgwcRA')
+      .set('response_type', 'code')
+      .set('state', 'test')
+      .set('redirect_uri', 'https://datarx-15696.web.app/')
+      .set('duration', 'permanent')
+      .set('scope', 'mysubreddits')
+
+    window.open('https://www.reddit.com/api/v1/authorize?' + params.toString());
+
+    /* */
   }
 }
